4cb7544ed6f7a266c18fb32a09cd0b7c888638c1
abdullahshah02 <abdullah.shah877@gmail.com>
2020-02-08 20:16:21 +0500

    iterative deepening added

--- RubikCube/RubikCube.cpp
+++ RubikCube/RubikCube.cpp
@@ -9,6 +9,12 @@ using namespace std;
 
 #define N 3
 
+int*** initCube();
+
+int*** initialCube = initCube();
+int*** finalCube = initCube();
+bool cubeSolved = false;
+
 //face 1: white (front)
 //face 2: yellow opposite to 1 (back)
 //face 3: orange (top)
@@ -17,7 +23,7 @@ using namespace std;
 //face 6 blue opposite to 5 (right)
 
 
-void inputCube(int cube[][3][3])
+void inputCube(int*** cube)
 {
 	for (int i = 0; i < 6; i++)
 		for (int j = 0; j < 3; j++)
@@ -25,7 +31,7 @@ void inputCube(int cube[][3][3])
 				cube[i][j][k] = i + 1;
 }
 
-void inputCubeFromFile(string fileName, int initialCube[][N][N], int finalCube[][N][N])
+void inputCubeFromFile(string fileName)
 {
 	ifstream file;
 	file.open(fileName);
@@ -37,19 +43,21 @@ void inputCubeFromFile(string fileName, int initialCube[][N][N], int finalCube[]
 
 	while (!file.eof())
 	{
-		for (int i = 0; i < 6; i++)
-			for (int j = 0; j < 3; j++)
-				for (int k = 0; k < 3; k++)
-					file >> initialCube[i][j][k];
-
+		//final cube is solved state
 		for (int i = 0; i < 6; i++)
 			for (int j = 0; j < 3; j++)
 				for (int k = 0; k < 3; k++)
 					file >> finalCube[i][j][k];
+
+		//initial cube is unsolved state
+		for (int i = 0; i < 6; i++)
+			for (int j = 0; j < 3; j++)
+				for (int k = 0; k < 3; k++)
+					file >> initialCube[i][j][k];
 	}
 }
 
-void printCube(int cube[6][3][3])
+void printCube(int*** cube)
 {
 	for (int i = 0; i < 6; i++)
 	{
@@ -68,7 +76,7 @@ void printCube(int cube[6][3][3])
 
 // An Inplace function to rotate a N x N matrix 
 // by 90 degrees in anti-clockwise direction 
-void rotateMatrixAntiClockwise(int mat[][N])
+void rotateMatrixAntiClockwise(int** mat)
 {
 	// Consider all squares one by one 
 	for (int x = 0; x < N / 2; x++)
@@ -95,25 +103,24 @@ void rotateMatrixAntiClockwise(int mat[][N])
 	}
 }
 
-void rotateMatrixClockwise(int a[N][N])
+void rotateMatrixClockwise(int** mat)
 {
-
 	// Traverse each cycle 
 	for (int i = 0; i < N / 2; i++) {
 		for (int j = i; j < N - i - 1; j++) {
 
 			// Swap elements of each cycle 
 			// in clockwise direction 
-			int temp = a[i][j];
-			a[i][j] = a[N - 1 - j][i];
-			a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
-			a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
-			a[j][N - 1 - i] = temp;
+			int temp = mat[i][j];
+			mat[i][j] = mat[N - 1 - j][i];
+			mat[N - 1 - j][i] = mat[N - 1 - i][N - 1 - j];
+			mat[N - 1 - i][N - 1 - j] = mat[j][N - 1 - i];
+			mat[j][N - 1 - i] = temp;
 		}
 	}
 }
 
-void moveCube(int cube[6][3][3], int faceNum, char move)
+void moveCube(int*** cube, int faceNum, char move)
 {
 	switch (move)
 	{
@@ -484,22 +491,518 @@ void moveCube(int cube[6][3][3], int faceNum, char move)
 	
 }
 
+int*** initCube() {
+	int*** newCube = new int**[6];
+
+	for (int i = 0; i < 6; i++) {
+		newCube[i] = new int*[3];
+
+		for (int j = 0; j < 3; j++)
+			newCube[i][j] = new int[3];
+	}
+	
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				newCube[i][j][k] = 0;
+			}
+		}
+	}
+
+	return newCube;
+}
+
+void deallocateCube(int*** cube) {
+	for (int i = 0; i != 6; ++i) 
+	{
+		for (int j = 0; j != 3; ++j){
+			delete[] cube[i][j];
+		}
+
+		delete[] cube[i];
+	}
+	delete[] cube;
+}
+
+int*** copyCube(int*** destCube, int*** srcCube) {
+
+	//Copy cube into newCube
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				destCube[i][j][k] = srcCube[i][j][k];
+			}
+		}
+	}
+	return destCube;
+}
+
+bool isEqual(int*** destCube, int*** srcCube) {
+	bool isEqual = true;
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 3; j++) {
+			for (int k = 0; k < 3; k++) {
+				if (destCube[i][j][k] != srcCube[i][j][k]) {
+					isEqual = false;
+					break;
+				}
+			}
+			if (!isEqual)
+				break;
+		}
+		if (!isEqual)
+			break;
+	}
+	return isEqual;
+}
+
+void rotateCubeClockwise(int*** cube, int face) {
+	int orange1;	int orange2;	int orange3;
+	int blue1;		int blue2;		int blue3;
+	int red1;		int red2;		int red3;
+	int green1;		int green2;		int green3;
+	int white1;		int white2;		int white3;
+	int yellow1;	int yellow2;	int yellow3;
+
+	switch (face) {
+	case 0:
+		rotateMatrixClockwise(cube[0]);
+		//store top row of orange, green, red and blue face
+		orange1 = cube[2][0][0]; red1 = cube[3][0][0]; green1 = cube[4][0][0]; blue1 = cube[5][0][0];
+		orange2 = cube[2][0][1]; red2 = cube[3][0][1]; green2 = cube[4][0][1]; blue2 = cube[5][0][1];
+		orange3 = cube[2][0][2]; red3 = cube[3][0][2]; green3 = cube[4][0][2]; blue3 = cube[5][0][2];
+
+
+		//first row of orange = first row of green
+		cube[2][0][0] = green1;
+		cube[2][0][1] = green2;
+		cube[2][0][2] = green3;
+
+		//first row of red = first row of blue
+		cube[3][0][0] = blue1;
+		cube[3][0][1] = blue2;
+		cube[3][0][2] = blue3;
+
+		//first row of green = first row of red
+		cube[4][0][0] = red1;
+		cube[4][0][1] = red2;
+		cube[4][0][2] = red3;
+
+		//first row of blue = first row of orange
+		cube[5][0][0] = orange1;
+		cube[5][0][1] = orange2;
+		cube[5][0][2] = orange3;
+
+		break;
+	case 1:
+		rotateMatrixClockwise(cube[1]);
+
+		orange1 = cube[2][2][0]; red1 = cube[3][2][0]; green1 = cube[4][2][0]; blue1 = cube[5][2][0];
+		orange2 = cube[2][2][1]; red2 = cube[3][2][1]; green2 = cube[4][2][1]; blue2 = cube[5][2][1];
+		orange3 = cube[2][2][2]; red3 = cube[3][2][2]; green3 = cube[4][2][2]; blue3 = cube[5][2][2];
+
+		//last row of orange = last row of blue
+		cube[2][2][0] = blue1;
+		cube[2][2][1] = blue2;
+		cube[2][2][2] = blue3;
+
+		//last row of red = last row of green
+		cube[3][2][0] = green1;
+		cube[3][2][1] = green2;
+		cube[3][2][2] = green3;
+
+		//last row of green = last row of orange
+		cube[4][2][0] = orange1;
+		cube[4][2][1] = orange2;
+		cube[4][2][2] = orange3;
+
+		//last row of blue = last row of red
+		cube[5][2][0] = red1;
+		cube[5][2][1] = red2;
+		cube[5][2][2] = red3;
+		break;
+	case 2:
+		rotateMatrixClockwise(cube[2]);
+
+		white1 = cube[0][0][0]; yellow1 = cube[1][0][0]; green1 = cube[4][0][0]; blue1 = cube[5][0][2];
+		white2 = cube[0][1][0]; yellow2 = cube[1][1][0]; green2 = cube[4][1][0]; blue2 = cube[5][1][2];
+		white3 = cube[0][2][0]; yellow3 = cube[1][2][0]; green3 = cube[4][2][0]; blue3 = cube[5][2][2];
+
+		//first column of white = last column of blue
+		cube[0][0][0] = blue1;
+		cube[0][1][0] = blue2;
+		cube[0][2][0] = blue3;
+
+		//first column of yellow = first column of green
+		cube[1][0][0] = green1;
+		cube[1][1][0] = green2;
+		cube[1][2][0] = green3;
+
+		//first column of green = first column of white
+		cube[4][0][0] = white1;
+		cube[4][1][0] = white2;
+		cube[4][2][0] = white3;
+
+		//last column of blue = first column of yellow
+		cube[5][0][0] = yellow1;
+		cube[5][1][0] = yellow2;
+		cube[5][2][0] = yellow3;
+		break;
+	case 3:
+		rotateMatrixClockwise(cube[3]);
+
+		white1 = cube[0][0][2]; yellow1 = cube[1][0][2]; green1 = cube[4][0][2]; blue1 = cube[5][0][0];
+		white2 = cube[0][1][2]; yellow2 = cube[1][1][2]; green2 = cube[4][1][2]; blue2 = cube[5][1][0];
+		white3 = cube[0][2][2]; yellow3 = cube[1][2][2]; green3 = cube[4][2][2]; blue3 = cube[5][2][0];
+
+		//last column of white = last column of green
+		cube[0][0][2] = green1;
+		cube[0][1][2] = green2;
+		cube[0][2][2] = green3;
+
+		//last column of yellow = first column of blue
+		cube[1][0][2] = blue1;
+		cube[1][1][2] = blue2;
+		cube[1][2][2] = blue3;
+
+		//last column of green = last column of yellow
+		cube[4][0][2] = yellow1;
+		cube[4][1][2] = yellow2;
+		cube[4][2][2] = yellow3;
+
+		//first column of blue = last column of white
+		cube[5][0][0] = white1;
+		cube[5][1][0] = white2;
+		cube[5][2][0] = white3;
+		break;
+
+	case 4:
+		rotateMatrixClockwise(cube[4]);
+
+		white1 = cube[0][2][0]; yellow1 = cube[1][0][0]; orange1 = cube[2][0][2]; red1 = cube[3][0][0];
+		white2 = cube[0][2][1]; yellow2 = cube[1][0][1]; orange2 = cube[2][1][2]; red2 = cube[3][1][0];
+		white3 = cube[0][2][2]; yellow3 = cube[1][0][2]; orange3 = cube[2][2][2]; red3 = cube[3][2][0];
+
+		//last row of white = last column of orange
+		cube[0][2][0] = orange1;
+		cube[0][2][1] = orange2;
+		cube[0][2][2] = orange3;
+
+		//first row of yellow = first column of red
+		cube[1][0][0] = red1;
+		cube[1][0][1] = red2;
+		cube[1][0][2] = red3;
+
+		//last column of orange = first row of yellow
+		cube[2][0][2] = yellow1;
+		cube[2][1][2] = yellow2;
+		cube[2][2][2] = yellow3;
+
+		//first column of red = last row of white
+		cube[3][0][0] = white1;
+		cube[3][1][0] = white2;
+		cube[3][2][0] = white3;
+		break;
+	case 5:
+		rotateMatrixClockwise(cube[5]);
+
+		white1 = cube[0][0][0]; yellow1 = cube[1][2][0]; orange1 = cube[2][0][0]; red1 = cube[3][0][2];
+		white2 = cube[0][0][1]; yellow2 = cube[1][2][1]; orange2 = cube[2][1][0]; red2 = cube[3][1][2];
+		white3 = cube[0][0][2]; yellow3 = cube[1][2][2]; orange3 = cube[2][2][0]; red3 = cube[3][2][2];
+
+		//first row of white = last column of red
+		cube[0][0][0] = red1;
+		cube[0][0][1] = red2;
+		cube[0][0][2] = red3;
+
+		//last row of yellow = first column of orange
+		cube[1][2][0] = orange1;
+		cube[1][2][1] = orange2;
+		cube[1][2][2] = orange3;
+
+		//first column of orange = first row of white
+		cube[2][0][0] = white1;
+		cube[2][1][0] = white2;
+		cube[2][2][0] = white3;
+
+		//last column of red = last row of yellow
+		cube[3][0][2] = yellow1;
+		cube[3][1][2] = yellow2;
+		cube[3][2][2] = yellow3;
+
+		break;
+	default:
+		break;
+	}
+}
+
+void rotateCubeCounterClockwise(int*** cube, int face) {
+	int orange1;	int orange2;	int orange3;
+	int blue1;		int blue2;		int blue3;
+	int red1;		int red2;		int red3;
+	int green1;		int green2;		int green3;
+	int white1;		int white2;		int white3;
+	int yellow1;	int yellow2;	int yellow3;
+
+	switch (face) {
+	case 0:
+		rotateMatrixClockwise(cube[0]);
+		//store top row of orange, green, red and blue face
+		orange1 = cube[2][0][0]; red1 = cube[3][0][0]; green1 = cube[4][0][0]; blue1 = cube[5][0][0];
+		orange2 = cube[2][0][1]; red2 = cube[3][0][1]; green2 = cube[4][0][1]; blue2 = cube[5][0][1];
+		orange3 = cube[2][0][2]; red3 = cube[3][0][2]; green3 = cube[4][0][2]; blue3 = cube[5][0][2];
+
+
+		//first row of orange = first row of blue
+		cube[2][0][0] = blue1;
+		cube[2][0][1] = blue2;
+		cube[2][0][2] = blue3;
+
+		//first row of red = first row of green
+		cube[3][0][0] = green1;
+		cube[3][0][1] = green2;
+		cube[3][0][2] = green3;
+
+		//first row of green = first row of orange
+		cube[4][0][0] = orange1;
+		cube[4][0][1] = orange2;
+		cube[4][0][2] = orange3;
+
+		//first row of blue = first row of red
+		cube[5][0][0] = red1;
+		cube[5][0][1] = red2;
+		cube[5][0][2] = red3;
+
+		break;
+	case 1:
+		rotateMatrixClockwise(cube[1]);
+
+		orange1 = cube[2][2][0]; red1 = cube[3][2][0]; green1 = cube[4][2][0]; blue1 = cube[5][2][0];
+		orange2 = cube[2][2][1]; red2 = cube[3][2][1]; green2 = cube[4][2][1]; blue2 = cube[5][2][1];
+		orange3 = cube[2][2][2]; red3 = cube[3][2][2]; green3 = cube[4][2][2]; blue3 = cube[5][2][2];
+
+		//last row of orange = last row of green
+		cube[2][0][0] = green1;
+		cube[2][0][1] = green2;
+		cube[2][0][2] = green3;
+
+		//last row of red = last row of blue
+		cube[3][0][0] = blue1;
+		cube[3][0][1] = blue2;
+		cube[3][0][2] = blue3;
+
+		//last row of green = last row of red
+		cube[4][0][0] = red1;
+		cube[4][0][1] = red2;
+		cube[4][0][2] = red3;
+
+		//last row of blue = last row of orange
+		cube[5][0][0] = orange1;
+		cube[5][0][1] = orange2;
+		cube[5][0][2] = orange3;
+		break;
+	case 2:
+		rotateMatrixClockwise(cube[2]);
+
+		white1 = cube[0][0][0]; yellow1 = cube[1][0][0]; green1 = cube[4][0][0]; blue1 = cube[5][0][2];
+		white2 = cube[0][1][0]; yellow2 = cube[1][1][0]; green2 = cube[4][1][0]; blue2 = cube[5][1][2];
+		white3 = cube[0][2][0]; yellow3 = cube[1][2][0]; green3 = cube[4][2][0]; blue3 = cube[5][2][2];
+
+		//first column of white = first column of green
+		cube[0][0][0] = green1;
+		cube[0][1][0] = green2;
+		cube[0][2][0] = green3;
+
+		//first column of yellow = last column of blue
+		cube[1][0][0] = blue1;
+		cube[1][1][0] = blue2;
+		cube[1][2][0] = blue3;
+
+		//first column of green = first column of yellow
+		cube[4][0][0] = yellow1;
+		cube[4][1][0] = yellow2;
+		cube[4][2][0] = yellow3;
+
+		//last column of blue = first column of white
+		cube[5][0][0] = white1;
+		cube[5][1][0] = white2;
+		cube[5][2][0] = white3;
+		break;
+	case 3:
+		rotateMatrixClockwise(cube[3]);
+
+		white1 = cube[0][0][2]; yellow1 = cube[1][0][2]; green1 = cube[4][0][2]; blue1 = cube[5][0][0];
+		white2 = cube[0][1][2]; yellow2 = cube[1][1][2]; green2 = cube[4][1][2]; blue2 = cube[5][1][0];
+		white3 = cube[0][2][2]; yellow3 = cube[1][2][2]; green3 = cube[4][2][2]; blue3 = cube[5][2][0];
+
+		//last column of white = first column of blue
+		cube[0][0][2] = blue1;
+		cube[0][1][2] = blue2;
+		cube[0][2][2] = blue3;
+
+		//last column of yellow = last column of green
+		cube[1][0][2] = green1;
+		cube[1][1][2] = green2;
+		cube[1][2][2] = green3;
+
+		//last column of green = last column of white
+		cube[4][0][2] = white1;
+		cube[4][1][2] = white2;
+		cube[4][2][2] = white3;
+
+		//first column of blue = last column of yellow
+		cube[5][0][0] = yellow1;
+		cube[5][1][0] = yellow2;
+		cube[5][2][0] = yellow3;
+		break;
+
+	case 4:
+		rotateMatrixClockwise(cube[4]);
+
+		white1 = cube[0][2][0]; yellow1 = cube[1][0][0]; orange1 = cube[2][0][2]; red1 = cube[3][0][0];
+		white2 = cube[0][2][1]; yellow2 = cube[1][0][1]; orange2 = cube[2][1][2]; red2 = cube[3][1][0];
+		white3 = cube[0][2][2]; yellow3 = cube[1][0][2]; orange3 = cube[2][2][2]; red3 = cube[3][2][0];
+
+		//last row of white = first column of red
+		cube[0][2][0] = red1;
+		cube[0][2][1] = red2;
+		cube[0][2][2] = red3;
+
+		//first row of yellow = last column of orange
+		cube[1][0][0] = orange1;
+		cube[1][0][1] = orange2;
+		cube[1][0][2] = orange3;
+
+		//last column of orange = last row of white
+		cube[2][0][2] = white1;
+		cube[2][1][2] = white2;
+		cube[2][2][2] = white3;
+
+		//first column of red = first row of yellow
+		cube[3][0][0] = yellow1;
+		cube[3][1][0] = yellow2;
+		cube[3][2][0] = yellow3;
+		break;
+	case 5:
+		rotateMatrixClockwise(cube[5]);
+
+		white1 = cube[0][0][0]; yellow1 = cube[1][2][0]; orange1 = cube[2][0][0]; red1 = cube[3][0][2];
+		white2 = cube[0][0][1]; yellow2 = cube[1][2][1]; orange2 = cube[2][1][0]; red2 = cube[3][1][2];
+		white3 = cube[0][0][2]; yellow3 = cube[1][2][2]; orange3 = cube[2][2][0]; red3 = cube[3][2][2];
+
+		//first row of white = first column of orange
+		cube[0][0][0] = orange1;
+		cube[0][0][1] = orange2;
+		cube[0][0][2] = orange3;
+
+		//last row of yellow = last column of red
+		cube[1][2][0] = red1;
+		cube[1][2][1] = red2;
+		cube[1][2][2] = red3;
+
+		//first column of orange = last row of yellow
+		cube[2][0][0] = yellow1;
+		cube[2][1][0] = yellow2;
+		cube[2][2][0] = yellow3;
+
+		//last column of red = first row of white
+		cube[3][0][2] = white1;
+		cube[3][1][2] = white2;
+		cube[3][2][2] = white3;
+
+		break;
+	default:
+		break;
+	}
+}
+
+int*** nextState(int*** srcCube, int faceNum, char direction) {
+	//Create new cube and copy the current cube into it
+	//Rotate the cube to get the next state
+	int*** newCube = initCube();
+	copyCube(newCube, srcCube);
+
+	if (direction == 'C')
+		rotateCubeClockwise(newCube, faceNum);
+	else if (direction == 'A')
+		rotateCubeCounterClockwise(newCube, faceNum);
+
+	return newCube;
+}
+
+bool deepSearch(int*** cube, int depth) {
+	//DFS
+	if (isEqual(cube, finalCube)) {
+		cout << "CUBE SOLVED" << endl;
+		cubeSolved = true;
+		return true;
+	}
+		
+	if (depth == 0)
+		return false;
+
+	for (int i = 0; i < 6; i++) {
+		int*** tempCube = nextState(cube, i, 'C');
+
+		if (deepSearch(tempCube, depth - 1)) {
+			cout <<"Face "<< i << " turned Clockwise" << endl;
+		}
+		deallocateCube(tempCube);
+		if (cubeSolved) return true;
+	}
+
+
+	for (int i = 0; i < 6; i++) {
+		int*** tempCube = nextState(cube, i, 'A');
+
+		if (deepSearch(tempCube, depth - 1)) {
+			cout << "Face " << i << " turned Counter Clockwise" << endl;
+		}
+		deallocateCube(tempCube);
+		if (cubeSolved) return true;
+	}
+	return false;
+}
+
+bool solveCube(int*** cube) {
+	//as rubiks cube can be solved in 20 moves
+	int max_depth = 20;
+	for (int i = 1; i < max_depth; i++) {
+		cout << "DEPTH " << i << endl;
+		if (deepSearch(cube, i)) {
+			//cout << "CUBE SOLVED AT " << i << " DEPTH" << endl;
+			return true;
+		}	
+		else {
+			cout << "UNABLE TO SOLVE IN " << i << " MOVES" << endl;
+		}
+	}
+	
+	return false;
+}
+
 int main()
 {
-	int initialCube[6][3][3];
-	int finalCube[6][3][3];
-
-	inputCubeFromFile("file.txt", initialCube, finalCube);
+	inputCubeFromFile("file.txt");
 
 	cout << "Initial Cube:" << endl;
 	printCube(initialCube);
 	cout << "Final Cube:" << endl;
 	printCube(finalCube);
 
-	cout << "Cube after a move:" << endl;
-	moveCube(initialCube, 0, 'A');
-	printCube(initialCube);
+	//rotateCubeClockwise(initialCube, 0);
+	//rotateCubeClockwise(initialCube, 0);
+	//rotateCubeClockwise(initialCube, 0);
 
+	//rotateCubeCounterClockwise(initialCube, 0);
+	//rotateCubeClockwise(initialCube, 1);
+	
+	solveCube(initialCube);
+	//printCube(initialCube);
+	deallocateCube(initialCube);
+	deallocateCube(finalCube);
 	return 0;
 }
 
