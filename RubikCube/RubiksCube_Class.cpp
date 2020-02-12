bb9d9eb7b4d75e545057efbbb5bdcadbb2fe5a3e
MuhammadUmarNaeem <m.umarnaeem7@gmail.com>
2020-02-12 21:51:30 +0500

    Fixed a bug in Rubik's Cube class' constructor

--- RubikCube/RubiksCube_Class.cpp
+++ RubikCube/RubiksCube_Class.cpp
@@ -7,7 +7,7 @@ RubiksCube_Class::RubiksCube_Class() {
 	for (int i = 0; i < 6; i++) {
 		for (int j = 0; j < 3; j++) {
 			for (int k = 0; k < 3; k++) {
-				cube[i][j][k] = i;
+				cube[i][j][k] = i + 1;
 			}
 		}
 	}
